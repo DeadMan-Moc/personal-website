f06da18706a07342e2e8441c173d4e114bc9ed19
DeadMan-MOC <mokgohloallie@gmail.com>
2019-07-22 10:15:24 +0200

    --- Auto-Git Commit ---

--- script/script.js
+++ script/script.js
@@ -1,3 +1,33 @@
 const menu = querySelector(".menu")
-const menuNav = ;
-const menuBranding = ;
+const menuNav = querySelector(".manu-nav");
+const menuBranding = querySelector(".menu-branding");
+const menuBtn = querySelector(".menu-btn")
+const navItems = querySelector(".nav-bar")
+
+//menu operations
+let showMenu = false;
+
+menuBtn = addEventListener("click", explode)
+
+function explodeMenu() {
+    if (!showMenu){
+        menuBtn.classList.add("close")
+        menuNav.classList.add("show")
+        menuBranding.classList.add("show")
+        menu.classList.add("show")
+
+        navItem.foreach (items => items.classList.add("show"));
+
+        showMenu = true;
+    }
+    else {
+        menuBtn.classList.remove("close")
+        menuNav.classList.a("show")
+        menuBranding.classList.add("show")
+        menu.classList.add("show")
+
+        navItem.foreach (items => items.classList.add("show"));
+
+        showMenu = true;
+    }
+}
